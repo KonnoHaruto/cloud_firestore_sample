7f0baacc84636b68c702e950f1a0fb456e68e5da
Haruto Konno <qingdoujinye@gmail.com>
2021-12-24 16:21:06 +0900

    feat(*): ListViewとAddingPageを追加

---
+++ lib/reference.dart
@@ -0,0 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+CollectionReference contentRef =
+    FirebaseFirestore.instance.collection('content');
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,13 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import '../reference.dart';
+import 'adding_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> _contentStream = contentRef.snapshots();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('playground'),
+      ),
+      body: SafeArea(
+          child: StreamBuilder(
+              stream: _contentStream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: Text('Loading...'),
+                  );
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return const Center(
+                    child: Text('Loading...'),
+                  );
+                }
+                return ListView(
+                  children:
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                    return ListTile(
+                      title: Text(data['name']),
+                      subtitle: Text(data['description']),
+                    );
+                  }).toList(),
+                );
+              })),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (_) {
+            return const AddingPage();
+          }));
+        },
       ),
     );
   }
